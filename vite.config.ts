8fcbc7f7693f7d2904b9c5e18415f25e0ef181b8
seunghoonKang <tmdgns4321@gmail.com>
2025-12-06 02:28:27 +0900

    feat - 배포 설정

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import react from '@vitejs/plugin-react-swc';
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    build: {
+      outDir: 'dist',
+      rollupOptions: {
+        input: './index.advanced.html',
+      },
+    },
+    base: process.env.GITHUB_PAGES === 'true' ? '/front_7th_chapter3-2/' : '/',
   }),
   defineTestConfig({
     test: {
